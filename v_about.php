aab3eed60201039df9a2f8eac6cd90777ad30bbf
Nununganna <90674127+Nununganna@users.noreply.github.com>
2021-09-29 17:04:59 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,25 @@
+<section>
+<h1><?php echo $judul ?></h1>
+
+<h4>Nama</h4>
+<ul type="disc">
+<li>Nama Depan : Nunung</li>
+<li>Nama Belakang : Siti Nurjanah</li>
+</ul>
+
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li> Jalan Sukarela Paninggilan Ciledug</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+<li>Yogyakarta</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+<li>Bulutangkis</li>
+<li>Catur</li>
+</ul>
+</section>
